1d2a5661078403b12041bacd72e212b1de42b73a
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-14 16:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function findLevel(){
+	let count=0;
+	let curr = document.getElementById("level");
+
+	while(curr !== null){
+		curr = curr.parentNode;
+		count++;
+	}
+	console.log(`The level of the element is: ${count}`);
+}
+findLevel()
